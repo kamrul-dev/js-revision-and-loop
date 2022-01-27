dc67d29eb062a70504559a0a5a20b04f1d94a488
Kamrul Hasan <you@example.com>
2022-01-28 02:02:00 +0600

    for loop and summary of this chapter

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+
+// var numbers = [23, 53, 35, 67, 36, 25, 95, 99];
+// for(var i = 0; i < numbers.length ; i++){
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+
+var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
+
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
