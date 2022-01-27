49dfe365eabb7fa134b338eb30ffa790ec4b9af8
Kamrul Hasan <you@example.com>
2022-01-28 01:41:16 +0600

    For loop, how for loop works, while vs for loop

---
+++ for-loop.js
@@ -0,0 +1,26 @@
+// var number = 1; //loop variable
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
+
+// for( var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast den, please !!');
+//     console.log(roastGiven);
+
+
+for (var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// odd number
+
+// for (var i = 1 ; i <= 20; i +=2){
+//     console.log(i);
+// }
+
+//even number
+// for (var i = 0 ; i <= 20; i +=2){
+//     console.log(i);
+// }
