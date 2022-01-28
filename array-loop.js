963fa5254187a49abe14abcac351fbedc9bbc4f9
Kamrul Hasan <you@example.com>
2022-01-28 16:06:07 +0600

    loop practice by problem

--- array-loop.js
+++ array-loop.js
@@ -6,9 +6,56 @@
 // }
 
 
-var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
+// var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
 
-for(var i = 0; i < items.length; i++){
-    var item = items[i];
-    console.log(item);
+// for(var i = 0; i < items.length; i++){
+//     var item = items[i];
+//     console.log(item);
+// }
+
+
+
+
+
+
+// var arrs = ['bat', 'ball,', 'stamp', 'helmate'];
+// for(var i = 0; i < arrs.length; i++){
+//    var arr = arrs[i];
+//     console.log(arr);
+
+// }
+
+// find out odd number from 1 to 20 using while loop
+// var oddNumber = 0;
+// while(oddNumber < 20){
+//     oddNumber = oddNumber + 1;
+//     console.log(oddNumber);
+//     oddNumber++;
+
+// }
+// find out even number from 1 to 20 using while loop
+// var evenNumber = 1;
+// while(evenNumber < 20){
+//     evenNumber = evenNumber + 1;
+//     console.log(evenNumber);
+//     evenNumber++;
+// }
+
+// find the sum of 1 to 20
+
+var sum = 0;
+var i = 1;
+while(i <= 20){
+  sum += i;
+  i++;
 }
+// console.log('The sum of natural numbers:', sum);
+
+// sum of natural numbers using for loop
+
+var sum = 0;
+for(var i = 1; i <= 20; i++){
+    sum = sum + i;
+}
+// console.log('Sum of natural numbers : ', sum);
+
